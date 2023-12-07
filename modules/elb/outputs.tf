aee96a90fb3ce8ef228371cae4a82928d114da9c
jyotsnaguptajain <jyots.scorpio24@gmail.com>
2023-12-07 21:53:32 +1100

    update: Displaying ALB DNS name on cli

--- modules/elb/outputs.tf
+++ modules/elb/outputs.tf
@@ -1,3 +1,7 @@
 output "target_group_arn" {
   value = aws_lb_target_group.my_tg.arn
 }
+
+output "alb_dns_name" {
+  value = aws_lb.sbs_alb.dns_name
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "alb_dns_name" {
+  value = module.my_alb.alb_dns_name
+}
