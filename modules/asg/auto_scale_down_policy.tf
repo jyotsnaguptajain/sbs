d797e7a19c72cf0893b6b2544d77a391b1075d42
jyotsnaguptajain <jyots.scorpio24@gmail.com>
2023-12-07 21:44:29 +1100

    update: Modified scaledown policy clodwatch alarm

--- modules/asg/auto_scale_down_policy.tf
+++ modules/asg/auto_scale_down_policy.tf
@@ -7,11 +7,19 @@ resource "aws_autoscaling_policy" "scale_down" {
   policy_type            = "SimpleScaling"
 }
 
-resource "aws_autoscaling_policy" "scale_down" {
-  name                   = "asg-scale-down"
-  autoscaling_group_name = aws_autoscaling_group.my_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "-1"
-  cooldown               = "300"
-  policy_type            = "SimpleScaling"
+resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
+  alarm_name          = "asg-scale-down-alarm"
+  alarm_description   = "asg-scale-down-cpu-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
 }
